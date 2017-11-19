cb49156bf4f6f1a55a56c85b9431d732099622d2
OsipXD <osip.fatkullin@gmail.com>
2017-11-19 11:00:54 +0300

    Fix save backpacks once

--- src/main/java/ru/endlesscode/rpginventory/event/listener/WorldListener.java
+++ src/main/java/ru/endlesscode/rpginventory/event/listener/WorldListener.java
@@ -23,6 +23,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldSaveEvent;
+
 import ru.endlesscode.rpginventory.inventory.InventoryManager;
 import ru.endlesscode.rpginventory.inventory.backpack.BackpackManager;
 
@@ -34,6 +35,10 @@ import ru.endlesscode.rpginventory.inventory.backpack.BackpackManager;
 public class WorldListener implements Listener {
     @EventHandler
     public void onWorldSave(WorldSaveEvent event) {
+        if (!event.getWorld().equals(Bukkit.getWorlds().get(0))) {
+            return;
+        }
+
         BackpackManager.saveBackpacks();
 
         for (Player player : Bukkit.getServer().getOnlinePlayers()) {
