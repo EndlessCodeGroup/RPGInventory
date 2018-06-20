79c6a993d18a4104ce330b1f0c2647afc133139a
OsipXD <osip.fatkullin@gmail.com>
2018-06-20 12:37:37 +0300

    Init commit

---
+++ bukkit/src/main/kotlin/ru/endlesscode/inspector/bukkit/Inspector.kt
@@ -0,0 +1,14 @@
+package ru.endlesscode.inspector.bukkit
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Inspector : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
