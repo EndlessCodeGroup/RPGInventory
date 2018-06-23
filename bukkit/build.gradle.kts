d36cd2e6ceafbcbc9bb9c7831b18a8ac8dbd50ae
OsipXD <osip.fatkullin@gmail.com>
2018-06-23 17:27:27 +0300

    Make Inspector simple JavaPlugin

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -4,34 +4,12 @@ import java.util.Properties
 plugins {
     id("com.github.johnrengelman.shadow") version "2.0.4"
     id("ru.endlesscode.bukkitgradle") version "0.8.0"
-    id("com.github.kukuhyoniatmoko.buildconfigkotlin") version "1.0.1"
     id("maven-publish")
 }
 
 val apiProject: Project by extra
 val minorVersion = 0
 version = "${apiProject.version}.$minorVersion"
-
-buildConfigKotlin {
-    val discordId: String
-    val discordToken: String
-
-    if (System.getenv("CI") == "true") {
-        discordId = System.getenv("DISCORD_ID")
-        discordToken = System.getenv("DISCORD_TOKEN")
-    } else {
-        val secretProps = Properties()
-        secretProps.load(file("secret.properties").inputStream())
-        discordId = secretProps.getProperty("discord.id", "SPECIFY_DISCORD_ID")
-        discordToken = secretProps.getProperty("discord.token", "SPECIFY_DISCORD_TOKEN")
-    }
-
-    sourceSet("main") {
-        packageName = "$group.${project.name}"
-        buildConfig(name = "DISCORD_ID", value = discordId)
-        buildConfig(name = "DISCORD_TOKEN", value = discordToken)
-    }
-}
 
 // TODO: Port it to Kotlin DSL
 apply(from = "groovy.gradle")
--- bukkit/src/main/kotlin/ru/endlesscode/inspector/bukkit/Inspector.kt
+++ bukkit/src/main/kotlin/ru/endlesscode/inspector/bukkit/Inspector.kt
@@ -1,16 +1,5 @@
 package ru.endlesscode.inspector.bukkit
 
-import ru.endlesscode.inspector.api.report.DiscordReporter
-import ru.endlesscode.inspector.api.report.Reporter
-import ru.endlesscode.inspector.bukkit.plugin.TrackedPlugin
+import org.bukkit.plugin.java.JavaPlugin
 
-class Inspector : TrackedPlugin(InspectorPlugin::class.java) {
-
-    override fun createReporter(): Reporter {
-        return DiscordReporter(
-                focus = this,
-                id = BuildConfig.DISCORD_ID,
-                token = BuildConfig.DISCORD_TOKEN
-        )
-    }
-}
+class Inspector : JavaPlugin()
