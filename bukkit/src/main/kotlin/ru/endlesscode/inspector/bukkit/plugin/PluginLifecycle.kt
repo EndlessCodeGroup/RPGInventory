4f17c4d9def6c2a84bfd19f48f4c092ca27c877d
OsipXD <osip.fatkullin@gmail.com>
2018-06-23 23:22:27 +0300

    Fix compatibility with lower than 1.12

--- bukkit/src/main/kotlin/ru/endlesscode/inspector/bukkit/plugin/PluginLifecycle.kt
+++ bukkit/src/main/kotlin/ru/endlesscode/inspector/bukkit/plugin/PluginLifecycle.kt
@@ -14,7 +14,7 @@ import java.io.InputStream
 import java.io.Reader
 import java.util.logging.Logger
 
-open class PluginLifecycle : PluginBase() {
+abstract class PluginLifecycle : PluginBase() {
 
     internal lateinit var holder: TrackedPlugin
 
