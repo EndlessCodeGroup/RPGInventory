637255f4a57beba7dac72a1ac98a2313d146f132
OsipXD <osip.fatkullin@gmail.com>
2018-08-21 14:01:27 +0300

    Add bukkit environment properties

--- bukkit/src/main/kotlin/ru/endlesscode/inspector/bukkit/report/BukkitEnvironment.kt
+++ bukkit/src/main/kotlin/ru/endlesscode/inspector/bukkit/report/BukkitEnvironment.kt
@@ -7,21 +7,36 @@ import ru.endlesscode.inspector.bukkit.Inspector
 import ru.endlesscode.inspector.bukkit.plugin.TrackedPlugin
 import ru.endlesscode.inspector.bukkit.util.printableForm
 
-class BukkitEnvironment(plugin: TrackedPlugin) : ReportEnvironment {
+
+class BukkitEnvironment(
+        plugin: Plugin,
+        properties: BukkitEnvironment.Properties
+) : ReportEnvironment {
 
     companion object {
         const val TAG_PLUGIN = "Plugin"
         const val TAG_CORE = "Server core"
         const val TAG_PLUGIN_LIST = "Installed plugins"
         const val TAG_INSPECTOR_VERSION = "Inspector version"
+
+        @JvmStatic
+        internal val EMPTY_PROPERTIES = Properties(emptyList())
     }
 
     override val fields = mapOf(
             TAG_PLUGIN to TextField(TAG_PLUGIN, plugin.printableForm),
             TAG_CORE to TextField(TAG_CORE,"${plugin.server.name} (${plugin.server.version})") { Inspector.GLOBAL.shouldSendData(DataType.CORE) },
-            TAG_PLUGIN_LIST to PluginListField(plugin.server.pluginManager.plugins.asList(), plugin.interestPluginsNames),
+            TAG_PLUGIN_LIST to PluginListField(plugin.server.pluginManager.plugins.asList(), properties.interestPluginsNames),
             TAG_INSPECTOR_VERSION to TextField(TAG_INSPECTOR_VERSION, Inspector.GLOBAL.version)
     )
 
     override val defaultFieldsTags: List<String> = listOf(TAG_PLUGIN, TAG_CORE, TAG_PLUGIN_LIST, TAG_INSPECTOR_VERSION)
+
+
+    /**
+     * Contains properties for environment customization
+     */
+    class Properties(
+            val interestPluginsNames: List<String>
+    )
 }
