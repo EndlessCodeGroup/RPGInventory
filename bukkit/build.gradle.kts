ae2ed2e24cdeb3eebd818c13a12d24a31f6ea8f6
OsipXD <osip.fatkullin@gmail.com>
2018-11-29 18:19:27 +0300

    Enable proguard on CI, update Shadow

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.jvm.tasks.Jar
 // Bukkit implementation build config
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "4.0.1"
+    id("com.github.johnrengelman.shadow") version "4.0.2"
     id("ru.endlesscode.bukkitgradle") version "0.8.0"
     id("maven-publish")
 }
@@ -15,6 +15,4 @@ version = "$inspectorVersion.$minorVersion"
 // TODO: Port it to Kotlin DSL
 apply(from = "groovy.gradle")
 
-if (System.getenv("CI") == null) {
-    apply(from = "proguard.gradle.kts")
-}
+apply(from = "proguard.gradle.kts")
