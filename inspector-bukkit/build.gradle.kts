9feb7f34123fdbbfde91f623e4dd99ab356c7ca9
OsipXD <osip.fatkullin@gmail.com>
2019-01-03 15:45:40 +0300

    refactor(Build): Build-script cleanup also remove powermock and jitpack

--- inspector-bukkit/build.gradle.kts
+++ inspector-bukkit/build.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.jvm.tasks.Jar
 plugins {
     id("com.github.johnrengelman.shadow") version "4.0.1"
     id("ru.endlesscode.bukkitgradle") version "0.8.1"
-    id("maven-publish")
+    `maven-publish`
 }
 
 // TODO: Port it to Kotlin DSL
