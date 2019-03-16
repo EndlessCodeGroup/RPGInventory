4b69a8e2232cb1436106481466d193e62ac7dd20
Osip Fatkullin <osip.fatkullin@gmail.com>
2019-03-16 12:00:20 +0300

    feat(Log): More friendly log message on durability parsing

--- src/main/java/ru/endlesscode/rpginventory/item/Texture.java
+++ src/main/java/ru/endlesscode/rpginventory/item/Texture.java
@@ -127,7 +127,7 @@ public class Texture {
                 item = ItemUtils.setTag(item, ItemUtils.HIDE_FLAGS_TAG, "63");
             }
         } catch (NumberFormatException e) {
-            Log.w("Can''t parse durability: {0}", e.toString());
+            Log.w("Can''t parse durability. Specify a number instead of \"{0}\"", durabilityValue);
         }
 
         return new Texture(item, durability);
